713c4b8a6639898fdbce946396e89b41b3dcb4f2
JulesFall <julesmangeat561@gmail.com>
2026-08-04 18:04:48 +0200

    Description des modifications

--- js/app.js
+++ js/app.js
@@ -8,3 +8,14 @@ const openMenu = () => {
         document.querySelector("header .icon").innerHTML = "⫶☰"
     }
 }
+
+const liens = document.querySelectorAll(".header-menu a");
+
+liens.forEach(lien => {
+    lien.addEventListener("click", () => {
+        const menu = document.querySelector(".header-menu");
+
+        menu.classList.remove("active");
+        document.querySelector("header .icon").innerHTML = "⫶☰";
+    });
+});
